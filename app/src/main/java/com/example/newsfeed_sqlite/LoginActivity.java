f94dbd570c566acd62b85cd09763ea4f528d5583
Кабанов Николай <isip_n.a.kabanov@mpt.ru>
2022-04-21 15:23:08 +0300

    add fingerprint scanner

--- app/src/main/java/com/example/newsfeed_sqlite/LoginActivity.java
+++ app/src/main/java/com/example/newsfeed_sqlite/LoginActivity.java
@@ -5,18 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.concurrent.Executor;
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText txtLogin, txtPassword;
     DatabaseHelper databaseHelper;
+
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,36 @@ public class LoginActivity extends AppCompatActivity {
         databaseHelper = new DatabaseHelper(this);
         txtLogin = findViewById(R.id.txtLogin);
         txtPassword = findViewById(R.id.txtPassword);
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(LoginActivity.this, executor, new BiometricPrompt.AuthenticationCallback(){
+
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Log.e("ErrorAUTH", errString.toString());
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                //Toast.makeText(LoginActivity.this, "Успешно!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(LoginActivity.this, FeedForAdminActivity.class).putExtra("Id", 2);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Log.e("FailedAUTH", "Fail!");
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Авторизация")
+                .setSubtitle("Прислоните палец")
+                .setNegativeButtonText("Отмена")
+                .build();
     }
 
     public void enterClick(View view) {
@@ -42,8 +83,9 @@ public class LoginActivity extends AppCompatActivity {
                     Intent intent = new Intent(this, FeedForAdminActivity.class).putExtra("Id", res.getInt(0));
                     startActivity(intent);
                 } else {
-                    Intent intent = new Intent(this, FeedForAdminActivity.class).putExtra("Id", res.getInt(0));
-                    startActivity(intent);
+                    biometricPrompt.authenticate(promptInfo);
+                    //Intent intent = new Intent(this, FeedForAdminActivity.class).putExtra("Id", res.getInt(0));
+                    //startActivity(intent);
                 }
             }
         }
